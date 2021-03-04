fb38800910e0f8774f73e2010a0da62f0a51cf25
moustafa <moustafamamdouh2013@gmail.com>
2021-03-04 22:53:22 +0200

    2nd Modification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
-
+import javax.swing.*;
+import  java.util.*;
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World !");
         System.out.println("change 1");
+        JOptionPane.showMessageDialog(null,"Warning !","MessageBox",JOptionPane.INFORMATION_MESSAGE);
     }
 }
